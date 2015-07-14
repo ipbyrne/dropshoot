c6e0ce81bb3f71bb7946b1aec035e2ca74f6de2b
ipbyrne <ipbyrne@IsaacParnesiMac.PK5001Z>
2015-07-14 11:06:21 -0600

    updated game with enemy projectiles, flying enemies and flying enemyprojectiles.

---
+++ js/prefabs/Enemy.js
@@ -0,0 +1,76 @@
+// Game Object
+var DropShoot = DropShoot || {};
+
+DropShoot.Enemy = function(game, x, y, key, health, enemyBullets)  {
+    Phaser.Sprite.call(this, game, x, y, key);
+    
+    this.game = game;
+    
+    this.health = health;
+    
+    this.enemyBullets = enemyBullets;
+    this.enemyTimer = this.game.time.create(false);
+    this.enemyTimer.start();
+    
+    
+};
+
+DropShoot.Enemy.prototype = Object.create(Phaser.Sprite.prototype);
+DropShoot.Enemy.prototype.constructor = DropShoot.Enemy;
+
+DropShoot.Enemy.prototype.update = function() {
+    
+    if(this.body.touching.down) {
+        if(this.body.velocity.x > 0 && this.x > this.sx + this.range || this.body.velocity.x < 0 && this.x < this.sx - this.range) {
+            this.body.velocity.x *= -1;
+            if(this.shooter == 1) {
+                this.bulletSpeed = this.body.velocity.x * -1;
+                this.scheduleShooting();
+            }
+        }
+    } 
+    
+    
+    if(this.y > 320) {
+        this.y = 0;
+        this.body.velocity.x = this.speed;
+        this.sx = this.x;
+    }
+    if(this.x < 12) {
+            this.x = 12;
+            this.body.velocity.x = this.speed;
+        }
+        if(this.x > 468) {
+            this.x = 468;
+            this.body.velocity.x = this.speed * -1;
+        }
+};
+
+DropShoot.Enemy.prototype.reset = function(x, y, health, key, scale, range, speed, shoot, time) {
+    Phaser.Sprite.prototype.reset.call(this, x, y, health);
+    this.loadTexture(key);
+    this.scale.setTo(scale);
+    this.speed = speed;
+    this.body.velocity.x = this.speed;
+    this.body.immoveable = true;
+    this.sx = x;
+    this.range = range;
+    this.shooter = shoot;
+    this.enemyTimer.resume();
+};
+
+DropShoot.Enemy.prototype.scheduleShooting = function() {
+    this.shoot();
+};
+
+DropShoot.Enemy.prototype.shoot = function() {
+    var ebullet = this.enemyBullets.getFirstExists(false);
+    if(!ebullet) {
+        ebullet = new DropShoot.EnemyBullet(this.game, this.x, this.y, this.bulletSpeed);
+        ebullet.allowGravity = false;
+        ebullet.immovable = true;
+        this.enemyBullets.add(ebullet);
+    } else {
+        ebullet.reset(this.x, this.y);
+    }
+};
